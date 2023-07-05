25a708c42c8580e533a89ef1c05d1f94810f1377
Masum-Reza <careerhunter4280@gmail.com>
2023-07-05 14:42:20 -0700

    router setup successfully

--- my-project-1/src/App.jsx
+++ my-project-1/src/App.jsx
@@ -6,39 +6,48 @@ import TextForm from './components/TextForm'
 import Alert from './components/Alert'
 import About from './components/About'
 
+import { BrowserRouter as Main, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   const [mode, setMode] = useState(`gray-300`)
-  let ToggleMode = ()=>{
-    if(mode==='black'){
+  let ToggleMode = () => {
+    if (mode === 'black') {
       setMode('gray-300')
       document.body.style.backgroundColor = 'white'
     }
-    else{
+    else {
       setMode('black')
       document.body.style.backgroundColor = '#282857'
     }
   }
 
-  const [alert, setAlerte] = useState(null)  
-  let HandleAlert = (type, message)=>{
+  const [alert, setAlerte] = useState(null)
+  let HandleAlert = (type, message) => {
     setAlerte({
       type: type,
-      message : message,
+      message: message,
     })
 
     setTimeout(() => {
       setAlerte(null)
     }, 2500);
   }
-  
+
   return (
-<>
-<Navbar mode={mode} toggleMode={ToggleMode}/>
-<Alert alert = {alert}/>
-<TextForm mode={mode} HandleAlert={HandleAlert}/>
-<About mode={mode}/>
-</>
+    <>
+      <Main>
+
+        <Navbar mode={mode} toggleMode={ToggleMode} />
+        <Alert alert={alert} />
+        <Routes>
+          <Route exact path='/' element={<TextForm mode={mode} HandleAlert={HandleAlert} />} />
+          <Route exact path='/about' element={<About mode={mode}/>} />
+
+        </Routes>
+
+      </Main>
+    </>
   )
 }
 
--- my-project-1/src/components/Navbar.jsx
+++ my-project-1/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 
 export default function Navbar(props) {
@@ -7,9 +8,9 @@ export default function Navbar(props) {
         <div>
             <nav className={`bg-${props.mode} text-${props.mode === 'black' ? 'white' : 'black'} font-bold`}>
                 <ul className={`h-12 flex space-x-5 items-center`}>
-                    <li className={` translate-x-10 hover:cursor-pointer hover:-translate-y-[0.13rem] active:translate-y-[0.13rem] shadow-md shadow-${props.mode==='gray-300'? 'red-400' : 'blue-500'}`}>TextUtils</li>
-                    <li className={` translate-x-10 hover:cursor-pointer hover:-translate-y-[0.13rem] active:translate-y-[0.13rem] shadow-md shadow-${props.mode==='gray-300'? 'red-400' : 'blue-500'}`}>User-Guide</li>
-                    <li className={` translate-x-10 hover:cursor-pointer hover:-translate-y-[0.13rem] active:translate-y-[0.13rem] shadow-md shadow-${props.mode==='gray-300'? 'red-400' : 'blue-500'}`}>Connect-Us</li>
+                    <li className={` translate-x-10 hover:cursor-pointer hover:-translate-y-[0.13rem] active:translate-y-[0.13rem] shadow-md shadow-${props.mode==='gray-300'? 'red-400' : 'blue-500'}`}> <Link to="/">TextUtils</Link></li>
+                    <li className={` translate-x-10 hover:cursor-pointer hover:-translate-y-[0.13rem] active:translate-y-[0.13rem] shadow-md shadow-${props.mode==='gray-300'? 'red-400' : 'blue-500'}`}> <Link to="#">User-Guide</Link> </li>
+                    <li className={` translate-x-10 hover:cursor-pointer hover:-translate-y-[0.13rem] active:translate-y-[0.13rem] shadow-md shadow-${props.mode==='gray-300'? 'red-400' : 'blue-500'}`}> <Link to="/about">About</Link></li>
                     <div className="form-check form-switch absolute right-0 lg:right-10 flex items-center space-x-3">
                         <input className="form-check-input hover:cursor-pointer bg-indigo-700 rounded-md " type="checkbox" role="switch" onClick={props.toggleMode} id="flexSwitchCheckDefault" />
                         <span className='hidden lg:flex'>Toggle Mode</span>
